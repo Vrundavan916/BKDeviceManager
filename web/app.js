fae9ae752951c20e08d9bafe890a94218d68b131
Vrundavan916 <vrundavanornaments.bharat@gmail.com>
2026-07-18 23:31:06 +0530

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,14 +1,11 @@
-function login(){
+function login() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
-let email=document.getElementById("email").value;
-let password=document.getElementById("password").value;
-
-if(email==="admin@bk.com" && password==="123456"){
-    document.getElementById("msg").style.color="green";
-    document.getElementById("msg").innerHTML="Login Successful";
-}else{
-    document.getElementById("msg").style.color="red";
-    document.getElementById("msg").innerHTML="Invalid Login";
-}
-
+    if (email === "admin@bk.com" && password === "123456") {
+        window.location.href = "dashboard.html";
+    } else {
+        document.getElementById("msg").style.color = "red";
+        document.getElementById("msg").innerHTML = "Invalid Email or Password";
+    }
 }
